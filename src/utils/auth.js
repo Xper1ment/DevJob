f6cd4769a6f2186839835bcdfb6789c34dc3a7d9
real-player <gdgsonu@gmail.com>
2021-09-13 20:36:39 +0530

    fix:jwt verification error

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -10,7 +10,7 @@ const newToken = user =>{
 
 const verifyToken = token =>{
     return new Promise(( resolve , reject ) => {
-        jwt.verify( token , `${process.env.ACCESS_TOKEN_SECREAT}` , (err , payload) =>{
+        jwt.verify( token , `${process.env.ACCESS_TOKEN_SECRET}` , (err , payload) =>{
                 if(err) return reject(err);
                 resolve(payload)
             })
